7a0481ba42b88e381d14b2069126e784c33aba4a
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-01-29 16:07:27 -0500

    Added setDate function

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360);
+    console.log(seconds);
+}
+setInterval(setDate, 1000);
