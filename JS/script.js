c4e7abf0e2729c6d5a9c13ae20b39587bb413dc9
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-01-29 16:11:44 -0500

    Added 90 to secondDegrees const

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ const secondHand = document.querySelector('.second-hand');
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360);
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
     console.log(seconds);
 }
